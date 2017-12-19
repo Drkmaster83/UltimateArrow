7c7a34d135a141d8b775ad136dafc270f7b4455f
Drkmaster83 <Drkmaster83@yahoo.com>
2017-12-18 23:31:18 -0600

    Fix errors

--- src/com/nightrosexl/ua/Commands.java
+++ src/com/nightrosexl/ua/Commands.java
@@ -7,55 +7,54 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-
 public class Commands implements CommandExecutor {
+	private UltimateArrow ua;
 
-    private UltimateArrow ua;
-    
-    public Commands(UltimateArrow ua) {
-        this.ua = ua;
-    }
-    
-    @Override
-    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        
-        if (!cmd.getName().equalsIgnoreCase("ua")) return false;
-        
-        Player p = (Player) sender;
-        
-        String playerName = p.getName();
-        String[] uaArgs = {"join", "leave"};
-        
-        if (args.length == 0) {
-            p.sendMessage(ChatColor.RED + ua.getPrefix() + "Additional arguments are required.");
-            p.sendMessage(ChatColor.RED + ua.getPrefix() + "Usage /ua [" + uaArgs[0] + ", " + uaArgs[1] + "]");
-            return true;
-        }
-        
-        if (args[0].equalsIgnoreCase(uaArgs[0])) {
-            ua.addToUAGeneralRoster(p, "");
-            p.sendMessage(ChatColor.DARK_GREEN + ua.getPrefix() + playerName + ", you will be teleported to the team selection area soon!");
+	public Commands(UltimateArrow ua) {
+		this.ua = ua;
+	}
 
-            // check team size, start game, if conditions are met.
-            // loop through list and teleport everyone in it.
-            if (ua.getUAGeneralPlayerRoster().size() >= 2/*ua.getUAGeneralPlayerRoster().size() >= 10 && ua.getUAGeneralPlayerRoster().size() <= 20*/) {
-                for (UAPlayer uap : ua.getUAGeneralPlayerRoster()) {
-                    uap.getPlayer().teleport(ua.getSelectArea());
-                }
-            }
-        } else if (args[0].equalsIgnoreCase(uaArgs[1])) {
-            ua.removeFromUAGeneralRoster(p);
-            
-            // TODO: remove lines 50-52, put them in a cleanup method of some sort.
-            p.getInventory().remove(Material.BOW);
-            p.getPlayer().teleport(ua.getSelectArea());
-            p.sendMessage(ChatColor.DARK_RED + ua.getPrefix() + playerName + ", you have been removed from the game!");
-        }
-        return true;
-    }
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if (!cmd.getName().equalsIgnoreCase("ua")) return false;
+
+		Player p = (Player) sender;
+
+		String playerName = p.getName();
+		String[] uaArgs = {"join", "leave"};
+
+		if (args.length == 0) {
+			p.sendMessage(ChatColor.RED + ua.getPrefix() + "Additional arguments are required.");
+			p.sendMessage(ChatColor.RED + ua.getPrefix() + "Usage /ua [" + uaArgs[0] + ", " + uaArgs[1] + "]");
+			return true;
+		}
+
+		if (args[0].equalsIgnoreCase(uaArgs[0])) {
+			ua.addToUAGeneralRoster(p, "");
+			p.sendMessage(ChatColor.DARK_GREEN + ua.getPrefix() + playerName + ", you will be teleported to the team selection area soon!");
+
+			// check team size, start game, if conditions are met.
+			// loop through list and teleport everyone in it.
+			if (ua.getUAGeneralPlayerRoster().size() >= 2/*ua.getUAGeneralPlayerRoster().size() >= 10 && ua.getUAGeneralPlayerRoster().size() <= 20*/) {
+				for (UAPlayer uap : ua.getUAGeneralPlayerRoster()) {
+					uap.getPlayer().teleport(ua.getSelectArea());
+				}
+			}
+		} else if (args[0].equalsIgnoreCase(uaArgs[1])) {
+			ua.removeFromUAGeneralRoster(p);
+
+			// TODO: remove lines 50-52, put them in a cleanup method of some sort.
+			p.getInventory().remove(Material.BOW);
+			p.getPlayer().teleport(ua.getSelectArea());
+			p.sendMessage(ChatColor.DARK_RED + ua.getPrefix() + playerName + ", you have been removed from the game!");
+		}
+		return true;
+	}
 }
 
-//TODO:
-// if /ua leave and you aren't in game, send message
-// if /ua solely, send message
-// if players leave the server, remove from game.
+/*
+TODO:
+ if /ua leave and you aren't in game, send message
+ if /ua solely, send message
+ if players leave the server, remove from game.
+*/
--- src/com/nightrosexl/ua/TTHandler.java
+++ src/com/nightrosexl/ua/TTHandler.java
@@ -8,7 +8,6 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 
-<<<<<<< HEAD
 public class TTHandler implements Listener {    
     private UltimateArrow ua;
     
@@ -20,21 +19,22 @@ public class TTHandler implements Listener {
     @EventHandler
     public void teamHandling(PlayerInteractEvent e) {
         Player p = e.getPlayer();
+        
         if (e.getAction() != Action.PHYSICAL) return;
-        if (e.getClickedBlock().getType() == Material.STONE_PLATE && ua.getPlayer(p) != null) { // If they stepped on a stone pressure plate and we've added them to our roster
+        if (e.getClickedBlock().getType() == Material.STONE_PLATE && ua.getPlayer(p) != null) { //If they stepped on a stone pressure plate and we've added them to our roster
             UAPlayer player = ua.getPlayer(p);
+            ua.getGameplay().checkReadyPeriod();
             if (e.getClickedBlock().getLocation().equals(ua.getRedPlate())) {
-                player.setTeam("Red"); // Set their team!
+                player.setTeam("Red"); //Set their team!
                 p.sendMessage(ChatColor.DARK_GREEN + ua.getPrefix() + p.getName() + ", you have joined the Red Team!");
-                p.teleport(ua.getRedSide());
+                p.teleport(ua.getRedSide()); //bad
             }
             
             if (e.getClickedBlock().getLocation().equals(ua.getBluePlate())) {
                 player.setTeam("Blue"); //Remember, we could also teleport the player to a location if we had a UAPlayer#joinGame() method
                 p.sendMessage(ChatColor.DARK_GREEN + ua.getPrefix() + p.getName() + ", you have joined the Blue Team!");
-                p.teleport(ua.getBlueSide());
+                p.teleport(ua.getBlueSide()); //bad
             }
-        
         // not in list and trigger plate? send message.
         } else if (ua.getPlayer(p) == null && (e.getClickedBlock().getLocation().equals(ua.getRedPlate()) || e.getClickedBlock().getLocation().equals(ua.getBluePlate()))) {
             p.sendMessage(ChatColor.RED + ua.getPrefix() +  "Please join the game to select a team!");
@@ -47,137 +47,26 @@ public class TTHandler implements Listener {
     public void readyUp(PlayerInteractEvent e) {
         Player p = e.getPlayer();
         
-        if (e.getClickedBlock() == null || e.getClickedBlock().getType() == Material.AIR) return;
-        if (ua.getPlayer(p) == null) return; // Player's not in-game/in-arena
+        if(e.getClickedBlock() == null || e.getClickedBlock().getType() == Material.AIR) return;
+        if(ua.getPlayer(p) == null) return; //Player's not in-game/in-arena
         UAPlayer player = ua.getPlayer(p);
         if(e.getClickedBlock().getType() == Material.STONE_BUTTON && !player.isReady()) {
             player.setReady(true);
             p.sendMessage(ChatColor.DARK_GREEN + ua.getPrefix() + p.getName() + ", you are ready!");
             Gameplay gp = ua.getGameplay();
-            gp.distributeEquipment(p);
-            if (gp.getReadyAmount() == ua.getUAGeneralPlayerRoster().size()) {
-            	gp.endReadyPeriod();
+            gp.distributeEquipment(p); //Give the bow
+            if(gp.getReadyAmount() == ua.getUAGeneralPlayerRoster().size()) {
+                gp.endReadyPeriod(); //When this is called, a randomly selected player will receive the arrow.
             }
-            
             // TODO: send message to everyone in match of players that are ready...
         }
     }
-=======
-import net.md_5.bungee.api.ChatColor;
-
-public class TTHandler implements Listener {
-
-	UltimateArrow ua;
-	Gameplay gp;
-	Player p;
-	Location redTeamPlateLoc, redTeamSide, blueTeamPlateLoc, blueTeamSide, uaTeamSelectArea, viewing_deck1;
-	World w;
-	
-	UUID playerUUID;
-	String uaPrefix = "[Ultimate Arrow] ";
-	String playerName;
-	
-	boolean isReady = false;
-	
-	public TTHandler(UltimateArrow ua) {
-		this.ua = ua;
-		gp = new Gameplay(this.ua);
-	}
-	
-	// team handling stuff...
-	@EventHandler
-	public void teamHandling(PlayerInteractEvent e) {
-		p = e.getPlayer();
-		w = p.getWorld();
-		playerUUID = p.getUniqueId();
-		playerName = p.getName();
-		redTeamPlateLoc = new Location(w, 408, 64, 636);
-        blueTeamPlateLoc = new Location(w, 404, 64, 632);
-        
-        	// check if block has been clicked first...
-        	if (e.getClickedBlock().getType() == Material.STONE_PLATE && ua.getUAGeneralPlayerRoster().contains(playerUUID)) {
-    			if (e.getClickedBlock().getLocation().equals(redTeamPlateLoc)) {
-    				ua.addToRedTeamRoster(playerUUID);
-    				p.sendMessage(ChatColor.DARK_GREEN + uaPrefix + playerName + ", you have joined the Red Team!");
-    				teleportPlayer(317, 65, -351);
-    			}
-    			
-    			if (e.getClickedBlock().getLocation().equals(blueTeamPlateLoc)) {
-    				ua.addToBlueTeamRoster(playerUUID);
-    				p.sendMessage(ChatColor.DARK_GREEN + uaPrefix + playerName + ", you have joined the Blue Team!");
-    				teleportPlayer(317, 65, -443);
-    			}
-    		
-    		// not in list and trigger plate? send message.
-        	} else if (!(ua.getUAGeneralPlayerRoster().contains(playerUUID)) && e.getClickedBlock().getLocation().equals(redTeamPlateLoc) || e.getClickedBlock().getLocation().equals(blueTeamPlateLoc)) {
-    				p.sendMessage(ChatColor.RED + uaPrefix +  "Please join the game to select a team!");
-    				p.sendMessage(ChatColor.RED + uaPrefix + "Usage: /ua join");
-    		}
-	}
-	
-	// ready up method
-	@EventHandler
-	public void readyUp(PlayerInteractEvent e) {
-		p = e.getPlayer();
-		
-		if (ua.getUAGeneralPlayerRoster().contains(playerUUID) && e.getClickedBlock().getType() == Material.STONE_BUTTON && !isReady) {
-			isReady = true;
-			p.sendMessage(ChatColor.DARK_GREEN + uaPrefix + playerName + ", you are ready!");
-			gp.distributeEquipment(p);
-			
-			// TODO: send message to everyone in match of players that are ready...
-		} //else {}
-		
-		// kick players after 1 minute, if not ready
-		Bukkit.getScheduler().scheduleSyncDelayedTask(ua, new Runnable() {
-			public void run() {
-				if (!isReady && ua.getUAGeneralPlayerRoster().contains(playerUUID)) {
-					ua.getUAGeneralPlayerRoster().remove(playerUUID);
-					teleportPlayer(316, 72, -342);
-					p.sendMessage(ChatColor.DARK_RED + uaPrefix + playerName + ", you have been removed from the match as you are not ready.");
-				}
-			}
-		}, 1200);
-	}
-
-
-	// teleport stuff...
-	public void teleportPlayer(int x, int y, int z) {
-		redTeamSide = new Location(w, 317, 65, -351, -179.7F, 1.2F);
-		blueTeamSide = new Location(w, 317, 65, -443);
-		uaTeamSelectArea = new Location(w, 406, 64, 634);
-		viewing_deck1 = new Location(w, 316, 72, -342);
-			
-		if (x == 317 && y == 65 && z == -351) {
-			p.teleport(redTeamSide);
-		}
-			
-		if (x == 317 && y == 65 && z == -443) {
-			p.teleport(blueTeamSide);
-		}
-			
-		if (x == 406 && y == 64 && z == 634) {
-			p.teleport(uaTeamSelectArea);
-		}
-			
-		if (x == 316 && y == 72 && z == -342) {
-			p.teleport(viewing_deck1);
-		}
-	}
-
-
->>>>>>> d11b82c7fed0e9d624402ba9fc274270831d3b9d
 }
 
 /*
-
 TTHandler is responsible for handling teleportation and team management aspects of the Ultimate Arrow mini-game.
 
 TODO:
 - check if player steps on pressure plates -DONE-
-<<<<<<< HEAD
 - tidy this class up and do some major refactoring.
 */
-=======
-*/
->>>>>>> d11b82c7fed0e9d624402ba9fc274270831d3b9d
